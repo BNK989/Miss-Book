100a37bf75aaf8ff9dad60c3e9a96ce2dfd9357c
Ben Klinovsky <bnk989@gmail.com>
2024-03-11 14:59:22 +0200

    add Navigate via reactRouter

--- react-template-proj/RootCmp.jsx
+++ react-template-proj/RootCmp.jsx
@@ -1,30 +1,36 @@
 const { useState } = React
+const Router = ReactRouterDOM.HashRouter
+const { Route, Routes } = ReactRouterDOM
 
-import { Nav } from './cmps/Nav.jsx'
+import { Header } from './cmps/Header.jsx'
 import { Home } from './pages/Home-index.jsx'
 import { About } from './pages/About-index.jsx'
 import { Books } from './pages/Books-index.jsx'
 
 export function App() {
-  const [page, setPage] = useState('books')
 
+  
   const onSetPage = (ev, page) => {
     ev.preventDefault()
-    console.log(page)
-    setPage(page)
+
   }
 
   return (
-    <section className="app">
-      <header className="app-header">
-        <h1>Miss Book</h1>
-        <Nav onSetPage={onSetPage} />
-      </header>
-      <main className="container">
-        {page === 'home' && <Home />}
-        {page === 'about' && <About />}
-        {page === 'books' && <Books />}
-      </main>
-    </section>
+    <Router>
+      <section className="app">
+        <Header />
+        <main className="container">
+          <Routes className="flex">
+            <Route path="/" element={<Home />}>Home</Route>
+            <Route path="/about" element={<About />}>
+              About
+            </Route>
+            <Route path="/books" element={<Books />}>
+              Books
+            </Route>
+          </Routes>
+        </main>
+      </section>
+    </Router>
   )
 }
---
+++ react-template-proj/cmps/Header.jsx
@@ -0,0 +1,17 @@
+const { NavLink } = ReactRouterDOM
+const { useNavigate } = ReactRouter
+
+export function Header() {
+
+	const navigate = useNavigate()
+
+    return <header className="app-header flex align-center justify-between">
+		<h1 onClick={()=> navigate('/')}>Miss Book</h1>
+    	<nav className="app-nav flex">
+		<NavLink to="/">Home</NavLink>
+		<NavLink to="/about">About</NavLink>
+		<NavLink to="/books">Books</NavLink>
+		</nav>
+    
+	  </header>
+}
